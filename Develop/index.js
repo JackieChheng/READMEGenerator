ee6636447d05cb1faa7f7f3c5d549ff3fbf1e17e
Jackie Chheng <jackiechheng@Jackies-MBP.T-mobile.com>
2023-11-04 17:44:27 -0600

    init function made and dist folder

--- Develop/index.js
+++ Develop/index.js
@@ -52,14 +52,17 @@ const questions = [
 
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Initializes app
 function init() {
-    
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md file . . .");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+    });
 }
 
 // Function call to initialize app
